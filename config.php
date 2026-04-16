dad178da1ace49a1a0b859e530d7deb5c5bef13e
Jenith2201 <62185491+Jenith2201@users.noreply.github.com>
2026-04-16 22:44:36 +0530

    Create config.php

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+define('PHONEPE_CLIENT_ID', 'SANDBOX_CLIENT_ID');
+define('PHONEPE_CLIENT_SECRET', 'SANDBOX_CLIENT_SECRET');
+define('PHONEPE_CLIENT_VERSION', 1);
+
+define('PHONEPE_OAUTH_URL', 'https://api-preprod.phonepe.com/apis/pg-sandbox/v1/oauth/token');
+define('PHONEPE_BASE_URL', 'https://api-preprod.phonepe.com/apis/pg-sandbox');
+
+
+#https://api.phonepe.com/apis/v1/oauth/token
+#https://api.phonepe.com/apis/pg
