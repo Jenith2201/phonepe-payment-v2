b1436c652651645bc1a43aa0138bb8717a4deb9c
Jenith2201 <62185491+Jenith2201@users.noreply.github.com>
2026-04-16 23:54:17 +0530

    config.php

--- config.php
+++ config.php
@@ -6,6 +6,8 @@ define('PHONEPE_CLIENT_VERSION', 1);
 define('PHONEPE_OAUTH_URL', 'https://api-preprod.phonepe.com/apis/pg-sandbox/v1/oauth/token');
 define('PHONEPE_BASE_URL', 'https://api-preprod.phonepe.com/apis/pg-sandbox');
 
+define('APP_BASE_URL', 'http://localhost/phonepe-v2'); // change in PROD
+define('PHONEPE_SUCCESS_PATH', '/payment-success.php');
 
 #https://api.phonepe.com/apis/v1/oauth/token
 #https://api.phonepe.com/apis/pg
